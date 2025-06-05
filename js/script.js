af24f376dcf55f006447f703e81654a6c6f51a24
michaelkassie <michaelkassie1@gmail.com>
2025-06-05 17:44:42 -0400

    sample pokemonList created

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
-alert("hello world");
+/*alert("hello world");
 let favoriteFood = "Pizza";
-document.write(favoriteFood);
+document.write(favoriteFood);*/
+
+let pokemonList=[];
+
+pokemonList= [{name:"bulbasaur",height:7,types: ["poison", grass]}, {name: "charamander", height: 6, types:["fire"]}, {name:"pikachu",height:4,types:["electric"]}];
+
